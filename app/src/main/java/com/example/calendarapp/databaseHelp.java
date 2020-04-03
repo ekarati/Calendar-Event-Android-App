f4d19f7e3e1470616452c51dc4e0f486543baeec
ekarati <erina.karati@gmail.com>
2020-04-03 23:31:15 +0530

    Added databaseHelp.java

--- app/src/main/java/com/example/calendarapp/databaseHelp.java
+++ app/src/main/java/com/example/calendarapp/databaseHelp.java
@@ -6,7 +6,6 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
-
 import java.sql.Date;
 
 public class databaseHelp extends SQLiteOpenHelper
